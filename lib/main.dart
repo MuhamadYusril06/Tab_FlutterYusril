3c5994b83af37ea9c82afa246c77d80c78f1ad68
abi036169 <abi036169@gmail.com>
2024-07-26 07:35:26 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,73 @@
 import 'package:flutter/material.dart';
 
+import './hal_headset.dart' as headset;
+import './hal_komputer.dart' as komputer;
+import './hal_radio.dart' as radio;
+import './hal_smartphone.dart' as hp;
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: MyApp(),
-    ),
-  );
+  runApp(MaterialApp(
+    title: "Tab Bar",
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  @override
+  void initState() {
+    _controller = TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My App'),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.amber,
+        title: Text("Daftar Elektronik"),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.smartphone), text: "SMARTPHONE"),
+            Tab(icon: Icon(Icons.computer), text: "KOMPUTER"),
+            Tab(icon: Icon(Icons.headset), text: "HEADSET"),
+            Tab(icon: Icon(Icons.radio), text: "RADIO"),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: TabBarView(
+        controller: _controller,
+        children: <Widget>[
+          hp.Smartphone(),
+          komputer.Komputer(),
+          headset.Headset(),
+          radio.Radio(),
+        ],
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.amber,
+        child: TabBar(
+          controller: _controller,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.smartphone), text: "SMARTPHONE"),
+            Tab(icon: Icon(Icons.computer), text: "KOMPUTER"),
+            Tab(icon: Icon(Icons.headset), text: "HEADSET"),
+            Tab(icon: Icon(Icons.radio), text: "RADIO"),
+          ],
+        ),
       ),
     );
   }
